b6dcde889a401ecdb07cf1e2383e69fe2b563368
Giovani Farias <riccog.25@gmail.com>
2022-09-19 21:12:05 -0300

    refactor: improve tests of cpf validator

--- src/cpf-validator.ts
+++ src/cpf-validator.ts
@@ -1,59 +1,39 @@
-export function validate (str: string) {
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
+function hasAllDigitsEqual (cpf: string) {
+  const [firstDigit] = cpf
+  return cpf.split("").every(digit => digit === firstDigit)
+}
 
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
-        }
+function cleanCpf (cpf: string) {
+  return cpf.replace(/\D/g, "")
+}
 
+function isValidLength (cpf: string) {
+  return cpf.length === 11
+}
 
-	} else return false;
+function extractDigit (cpf: string) {
+  return cpf.slice(9);  
+}
 
+function calculateDigit (cpf: string, factor: number) {
+  let total = 0
+  for (const digit of cpf) {
+    if (factor > 1) {
+      total += parseInt(digit) * factor--
+    }   
+  }
+  const rest = total % 11
+  return (rest < 2) ? 0 : 11 - rest
+}
+
+export function validate (cpf: string) {
+	if (!cpf) return false
+    cpf = cleanCpf(cpf)
+    if (!isValidLength(cpf)) return false
+    if (hasAllDigitsEqual(cpf)) return false
+    const firstDigit = calculateDigit(cpf, 10) 
+    const secondDigit = calculateDigit(cpf, 11)
+    const checkDigit = extractDigit(cpf)
+    const calculatedDigit = `${firstDigit}${secondDigit}`  
+    return checkDigit == calculatedDigit;
 }
